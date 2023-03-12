9655a969fbfe5bd3b06303dc45935137d8607eab
Stanislav <people.ss@yandex.ru>
2023-03-13 00:11:29 +0300

    Corrected commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int balance = 48;
+        int amount = 68423;
+        boolean adding = amount >= 1000;
+
+        int percent;
+        if ( adding ) {
+            percent = 1;
+        } else {
+            percent = 0;
+        }
+
+        int bonus = amount / 100 * percent + balance;
+
+        System.out.println(bonus);
     }
 }
